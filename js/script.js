d2b33c9160f788ea1dd651d1ea1cbeef8d5dbc59
AlbertNotAlien <albert830207@gmail.com>
2023-03-02 22:54:09 +0800

    feat: complete click event in about block

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
-123123123123123;
-123123123;
+const aboutOptions = document.querySelectorAll(".about-option-text");
+
+// console.log("aboutOptions", aboutOptions);
+
+aboutOptions.forEach((option, optionIndex) => {
+  option.addEventListener("click", () => {
+    aboutOptions.forEach(function (item, selectedIndex) {
+      document.querySelector(".about-index-text").innerHTML = `/ 0${
+        selectedIndex + 1
+      }`;
+
+      if (optionIndex === selectedIndex) {
+        item.classList.add("about-option-text_active");
+      } else {
+        item.classList.remove("about-option-text_active");
+      }
+    });
+  });
+});
+
+function addClassList() {}
